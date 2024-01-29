3dff9f92e0eee5e3ed2cafd965893f7de301457c
Petro Podolets <petropodolets7@gmail.com>
2024-01-29 19:53:19 +0100

    hw-js-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,17 @@
 'use strict';
+function filterArray(numbers, value) {
+    const numberArray = [];
 
-function getElementWidth(content, padding, border) {
-    return parseFloat(content) + 2 * (parseFloat(padding) + parseFloat(border));
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            numberArray.push(numbers[i])
+        }
+    }
+    return numberArray;
 }
 
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
